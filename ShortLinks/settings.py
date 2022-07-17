9e2693d53a68b24c6d6f82c60ed26b16a25eaec8
Sakhm3t <volha.m.volkava@gmail.com>
2022-07-17 11:15:35 +0300

    ALLOWED_HOSTS was set

--- ShortLinks/settings.py
+++ ShortLinks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-be925)x-dju!%v+u97xdjny8w#8k6l2p@+fhn%1ypvrde-o)3q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'linkcutter.herokuapp.com']
 
 
 # Application definition

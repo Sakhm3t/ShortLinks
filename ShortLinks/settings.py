baeb5d94a5042524b59bc73c072abdf1fa371075
Sakhm3t <volha.m.volkava@gmail.com>
2022-07-17 15:45:14 +0300

    .gitignore & debug set to off for production

--- ShortLinks/settings.py
+++ ShortLinks/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-be925)x-dju!%v+u97xdjny8w#8k6l2p@+fhn%1ypvrde-o)3q')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = ('SECRET_KEY' not in os.environ)  # I assume that production deployment has it own key
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'linkcutter.herokuapp.com']
 

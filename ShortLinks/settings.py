2bdf4d9694e7e3a5a69b58382c1652f0a746a2ef
Sakhm3t <volha.m.volkava@gmail.com>
2022-07-17 13:58:25 +0300

    db engine was switched to postgres

--- ShortLinks/settings.py
+++ ShortLinks/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,11 +76,27 @@ WSGI_APPLICATION = 'ShortLinks.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'linkcutterdb',
+
+        'USER': 'django',
+
+        'PASSWORD': 'django',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
 }
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
